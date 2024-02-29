1ae5197954d093aec7d4c31fddf701215f410ac5
Crowdbotics <engineering@crowdbotics.com>
2024-02-29 18:43:24 +0000

    Update store.

--- store/avanadedemoappAPI/api.js
+++ store/avanadedemoappAPI/api.js
@@ -1,76 +1,100 @@
-import axios from "axios";
+import axios from "axios"
 const avanadedemoappAPI = axios.create({
   baseURL: "https://avanade-demo-app-46721.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function api_docs_schema_retrieve(payload) {
   return avanadedemoappAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
+    params: { lang: payload.lang }
+  })
 }
-
 function api_v1_login_create(payload) {
-  return avanadedemoappAPI.post(`/api/v1/login/`, payload);
+  return avanadedemoappAPI.post(`/api/v1/login/`, payload)
 }
-
 function api_v1_signup_create(payload) {
-  return avanadedemoappAPI.post(`/api/v1/signup/`, payload);
+  return avanadedemoappAPI.post(`/api/v1/signup/`, payload)
 }
-
+function modules_social_auth_apple_connect_create(payload) {
+  return avanadedemoappAPI.post(`/modules/social-auth/apple/connect/`, payload)
+}
+function modules_social_auth_apple_login_create(payload) {
+  return avanadedemoappAPI.post(`/modules/social-auth/apple/login/`, payload)
+}
+function modules_social_auth_facebook_connect_create(payload) {
+  return avanadedemoappAPI.post(
+    `/modules/social-auth/facebook/connect/`,
+    payload
+  )
+}
+function modules_social_auth_facebook_login_create(payload) {
+  return avanadedemoappAPI.post(`/modules/social-auth/facebook/login/`, payload)
+}
+function modules_social_auth_google_connect_create(payload) {
+  return avanadedemoappAPI.post(`/modules/social-auth/google/connect/`, payload)
+}
+function modules_social_auth_google_login_create(payload) {
+  return avanadedemoappAPI.post(`/modules/social-auth/google/login/`, payload)
+}
+function modules_social_auth_socialaccounts_list(payload) {
+  return avanadedemoappAPI.get(`/modules/social-auth/socialaccounts/`)
+}
+function modules_social_auth_socialaccounts_disconnect_create(payload) {
+  return avanadedemoappAPI.post(
+    `/modules/social-auth/socialaccounts/${payload.id}/disconnect/`,
+    payload
+  )
+}
 function rest_auth_login_create(payload) {
-  return avanadedemoappAPI.post(`/rest-auth/login/`, payload);
+  return avanadedemoappAPI.post(`/rest-auth/login/`, payload)
 }
-
 function rest_auth_logout_create(payload) {
-  return avanadedemoappAPI.post(`/rest-auth/logout/`);
+  return avanadedemoappAPI.post(`/rest-auth/logout/`)
 }
-
 function rest_auth_password_change_create(payload) {
-  return avanadedemoappAPI.post(`/rest-auth/password/change/`, payload);
+  return avanadedemoappAPI.post(`/rest-auth/password/change/`, payload)
 }
-
 function rest_auth_password_reset_create(payload) {
-  return avanadedemoappAPI.post(`/rest-auth/password/reset/`, payload);
+  return avanadedemoappAPI.post(`/rest-auth/password/reset/`, payload)
 }
-
 function rest_auth_password_reset_confirm_create(payload) {
-  return avanadedemoappAPI.post(`/rest-auth/password/reset/confirm/`, payload);
+  return avanadedemoappAPI.post(`/rest-auth/password/reset/confirm/`, payload)
 }
-
 function rest_auth_registration_create(payload) {
-  return avanadedemoappAPI.post(`/rest-auth/registration/`, payload);
+  return avanadedemoappAPI.post(`/rest-auth/registration/`, payload)
 }
-
 function rest_auth_registration_resend_email_create(payload) {
-  return avanadedemoappAPI.post(`/rest-auth/registration/resend-email/`, payload);
+  return avanadedemoappAPI.post(
+    `/rest-auth/registration/resend-email/`,
+    payload
+  )
 }
-
 function rest_auth_registration_verify_email_create(payload) {
-  return avanadedemoappAPI.post(`/rest-auth/registration/verify-email/`, payload);
+  return avanadedemoappAPI.post(
+    `/rest-auth/registration/verify-email/`,
+    payload
+  )
 }
-
 function rest_auth_user_retrieve(payload) {
-  return avanadedemoappAPI.get(`/rest-auth/user/`);
+  return avanadedemoappAPI.get(`/rest-auth/user/`)
 }
-
 function rest_auth_user_update(payload) {
-  return avanadedemoappAPI.put(`/rest-auth/user/`, payload);
+  return avanadedemoappAPI.put(`/rest-auth/user/`, payload)
 }
-
 function rest_auth_user_partial_update(payload) {
-  return avanadedemoappAPI.patch(`/rest-auth/user/`, payload);
+  return avanadedemoappAPI.patch(`/rest-auth/user/`, payload)
 }
-
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
   api_v1_signup_create,
+  modules_social_auth_apple_connect_create,
+  modules_social_auth_apple_login_create,
+  modules_social_auth_facebook_connect_create,
+  modules_social_auth_facebook_login_create,
+  modules_social_auth_google_connect_create,
+  modules_social_auth_google_login_create,
+  modules_social_auth_socialaccounts_list,
+  modules_social_auth_socialaccounts_disconnect_create,
   rest_auth_login_create,
   rest_auth_logout_create,
   rest_auth_password_change_create,
@@ -82,4 +106,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-};
+}
