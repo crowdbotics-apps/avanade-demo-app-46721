5d76796f548f88f275f28a18816b3efb376a098a
Chris LloydJones <chris.lloyd-jones@avanade.com>
2024-02-29 10:39:06 -0800

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "avanade-demo-app-46721",
   url: "https://avanade-demo-app-46721.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
